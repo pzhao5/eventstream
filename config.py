d8d36627ea0485c4c7578ec44081b7ef9e4ecbfc
Philip Zhao <pzhao5@users.noreply.github.com>
2014-12-02 09:06:40 -0500

    Update and rename config.py.sample to config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+
+config = {
+    'title': 'HackNyc24hr',
+    'heading_text': 'Hack',
+    'search_term': '#hack4humanity',
+    'twitter_application_key': 'Jdwy3P6b3L64MsiFVRAZeQibv',
+    'twitter_application_secret': '0LFhyyPGvMfuL91Ezfc3BzImsNpZf5ap6Zrh0CSjR0IXbMcRBA',
+    'google_api_key': 'google api key here',
+    'handlerconfig': {
+        'webapp2_extras.sessions': {
+            'secret_key': 'makeuparandomstring'
+        }
+    }
+}
